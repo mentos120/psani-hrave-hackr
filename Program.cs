e3c91802115301d22301197712aa119c2f165c6f
mentos120 <fabihokolopo@email.cz>
2023-03-08 15:05:26 +0100

    V2 - screenshotting

--- Program.cs
+++ Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace psani_hrave_hackr
 {
@@ -10,7 +15,15 @@ namespace psani_hrave_hackr
     {
         static void Main()
         {
-            
+            CaptureScreen();
+        }
+        static void CaptureScreen()
+        {
+            Rectangle rect = new Rectangle(1531, 306, 803, 407);
+            Bitmap bmp = new Bitmap(rect.Width, rect.Height, PixelFormat.Format32bppArgb);
+            Graphics g = Graphics.FromImage(bmp);
+            g.CopyFromScreen(rect.Left, rect.Top, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
+            bmp.Save("C:\\Users\\fabih\\Pictures\\psanihrave\\img.jpg", ImageFormat.Jpeg);
         }
     }
 }
