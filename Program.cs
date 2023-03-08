26c5a92ecc2e2ffbd6a51d4429eebd3116bb4592
mentos120 <fabihokolopo@email.cz>
2023-03-08 19:53:49 +0100

    V4

--- Program.cs
+++ Program.cs
@@ -9,27 +9,84 @@ using System.Drawing.Imaging;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using WindowsInput;
-using WindowsInput.Native;
+using Tesseract;
 
 namespace psani_hrave_hackr
 {
-    internal class Program
+    internal static class Program
     {
-        
-        InputSimulator sim = new InputSimulator();
-        void Main()
+        static void Main()
         {
-            sim.Keyboard.ModifiedKeyStroke(VirtualKeyCode.MENU, VirtualKeyCode.TAB);
-            sim.Keyboard.KeyPress(VirtualKeyCode.SPACE);
-            CaptureScreen();
+            Console.WriteLine("TP/O [test spani / ostatni psani");
+            string choose = Console.ReadLine();
+            new InputSimulator().Keyboard
+                .KeyDown(VirtualKeyCode.MENU)
+                .ModifiedKeyStroke(VirtualKeyCode.TAB, VirtualKeyCode.TAB)
+                .Sleep(500)
+                .KeyUp(VirtualKeyCode.MENU) 
+                .Sleep(500) 
+                .KeyPress(VirtualKeyCode.SPACE)
+                .Sleep(800);
+            CaptureScreen(choose);
+        }   
+        static void CaptureScreen(string choose)
+        {
+            if (choose == "TP")
+            {
+                Rectangle rect = new Rectangle(1500, 300, 820, 402);
+                Bitmap bmp = new Bitmap(rect.Width, rect.Height, PixelFormat.Format32bppArgb);
+                Graphics g = Graphics.FromImage(bmp);
+                g.CopyFromScreen(rect.Left, rect.Top, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
+                bmp.Save("C:\\Users\\fabih\\Pictures\\psanihrave\\img.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+                OCTresolve();
+            }
+            else if (choose == "O")
+            {
+                Rectangle rect = new Rectangle(1608, 339, 820, 420);
+                Bitmap bmp = new Bitmap(rect.Width, rect.Height, PixelFormat.Format32bppArgb);
+                Graphics g = Graphics.FromImage(bmp);
+                g.CopyFromScreen(rect.Left, rect.Top, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
+                bmp.Save("C:\\Users\\fabih\\Pictures\\psanihrave\\img.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+                OCTresolve();
+            }
+            else if (choose == "TGT")
+            {
+                TGTResolve();
+            }
         }
-        void CaptureScreen()
+        static void TGTResolve()
         {
-            Rectangle rect = new Rectangle(1531, 306, 803, 407);
-            Bitmap bmp = new Bitmap(rect.Width, rect.Height, PixelFormat.Format32bppArgb);
-            Graphics g = Graphics.FromImage(bmp);
-            g.CopyFromScreen(rect.Left, rect.Top, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
-            bmp.Save("C:\\Users\\fabih\\Pictures\\psanihrave\\img.jpg", ImageFormat.Jpeg);
+            for (int i = 0; i <= 20; i++)
+            {
+                Rectangle rect = new Rectangle(1608, 339, 820, 420);
+                Bitmap bmp = new Bitmap(rect.Width, rect.Height, PixelFormat.Format32bppArgb);
+                Graphics g = Graphics.FromImage(bmp);
+                g.CopyFromScreen(rect.Left, rect.Top, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
+                bmp.Save("C:\\Users\\fabih\\Pictures\\psanihrave\\img.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+                OCTresolve();
+            }            
+        }
+        static void WriteText(char[] chars)
+        {
+            foreach(char c in chars)
+            {
+                new InputSimulator().Keyboard
+                .TextEntry(c.ToString())
+                .Sleep(10);
+            }
+        }
+        static void OCTresolve()
+        {
+            var engine = new TesseractEngine("C:\\Users\\fabih\\source\\repos\\psani hrave hackr","ces", EngineMode.Default);
+            var img = Pix.LoadFromFile("C:\\Users\\fabih\\Pictures\\psanihrave\\img.bmp");
+            var page = engine.Process(img);
+            var text = page.GetText();
+            Console.WriteLine(text);
+            string cleanText = text.Replace("\n", " ").Replace("  ", " ").Remove(0, 2);
+            char[] chars = cleanText.ToCharArray();
+            Console.WriteLine();
+            foreach (char c in chars) { Console.Write(c); }
+            WriteText(chars);
         }
     }
 }
